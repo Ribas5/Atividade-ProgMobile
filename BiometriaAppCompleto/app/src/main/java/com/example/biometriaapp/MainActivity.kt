04f0c170f4a35e66c11cd633da38f69cf742585e
Ribas5 <51717679+Ribas5@users.noreply.github.com>
2025-06-08 17:49:00 -0300

    Atividade

---
+++ BiometriaAppCompleto/app/src/main/java/com/example/biometriaapp/MainActivity.kt
@@ -0,0 +1,53 @@
+package com.example.biometriaapp
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.biometric.BiometricManager
+import androidx.biometric.BiometricPrompt
+import androidx.core.content.ContextCompat
+import java.util.concurrent.Executor
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var executor: Executor
+    private lateinit var biometricPrompt: BiometricPrompt
+    private lateinit var promptInfo: BiometricPrompt.PromptInfo
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        executor = ContextCompat.getMainExecutor(this)
+        biometricPrompt = BiometricPrompt(this, executor, object : BiometricPrompt.AuthenticationCallback() {
+            override fun onAuthenticationSucceeded(result: BiometricPrompt.AuthenticationResult) {
+                super.onAuthenticationSucceeded(result)
+                runOnUiThread {
+                    Toast.makeText(applicationContext, "Autenticado com sucesso!", Toast.LENGTH_SHORT).show()
+                    setContentView(R.layout.activity_main)
+                }
+            }
+
+            override fun onAuthenticationFailed() {
+                super.onAuthenticationFailed()
+                runOnUiThread {
+                    Toast.makeText(applicationContext, "Autenticação falhou", Toast.LENGTH_SHORT).show()
+                }
+            }
+        })
+
+        val biometricManager = BiometricManager.from(this)
+        if (biometricManager.canAuthenticate(BiometricManager.Authenticators.BIOMETRIC_STRONG) == BiometricManager.BIOMETRIC_SUCCESS) {
+            promptInfo = BiometricPrompt.PromptInfo.Builder()
+                .setTitle("Autenticação Biométrica")
+                .setSubtitle("Use sua digital para continuar")
+                .setNegativeButtonText("Cancelar")
+                .setAllowedAuthenticators(BiometricManager.Authenticators.BIOMETRIC_STRONG)
+                .build()
+            biometricPrompt.authenticate(promptInfo)
+        } else {
+            Toast.makeText(this, "Biometria não disponível", Toast.LENGTH_SHORT).show()
+        }
+    }
+}
